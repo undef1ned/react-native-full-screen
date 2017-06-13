00c94a2c3d9c3f5099d6b7b071b1baed3b1aed05
undef1ned <the.cooljames@gmail.com>
2017-06-13 14:33:40 +0300

    Added activity instance check

--- android/src/main/java/com/rn/full/screen/FullScreen.java
+++ android/src/main/java/com/rn/full/screen/FullScreen.java
@@ -55,14 +55,17 @@ public class FullScreen extends ReactContextBaseJavaModule {
                 new Runnable() {
                     @Override
                     public void run() {
-                        getCurrentActivity().getWindow().getDecorView().setSystemUiVisibility(
-                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        );
+                        final Activity activity = getCurrentActivity();
+                        if (activity != null) {
+                            activity.getWindow().getDecorView().setSystemUiVisibility(
+                                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                            | View.SYSTEM_UI_FLAG_IMMERSIVE
+                            );
+                        }
                     }
                 }
         );
@@ -76,11 +79,14 @@ public class FullScreen extends ReactContextBaseJavaModule {
         UiThreadUtil.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                getCurrentActivity().getWindow().getDecorView().setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                );
+                final Activity activity = getCurrentActivity();
+                if (activity != null) {
+                    activity.getWindow().getDecorView().setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    );
+                }
             }
         });
 
