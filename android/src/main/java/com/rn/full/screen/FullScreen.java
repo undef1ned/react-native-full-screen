5adc57536f44a6e6886a6c778ca38b9af70852df
undef1ned <the.cooljames@gmail.com>
2017-06-13 14:52:10 +0300

    Fixed window decoration options

--- android/src/main/java/com/rn/full/screen/FullScreen.java
+++ android/src/main/java/com/rn/full/screen/FullScreen.java
@@ -64,6 +64,7 @@ public class FullScreen extends ReactContextBaseJavaModule {
                                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                                             | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                                             | View.SYSTEM_UI_FLAG_IMMERSIVE
+                                            | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
                             );
                         }
                     }
@@ -83,8 +84,7 @@ public class FullScreen extends ReactContextBaseJavaModule {
                 if (activity != null) {
                     activity.getWindow().getDecorView().setSystemUiVisibility(
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
                     );
                 }
             }
